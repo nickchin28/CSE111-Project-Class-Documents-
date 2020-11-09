9bd6a33bc68d1defdcbffb68238f23480523dffa
nickchin28 <36184374+nickchin28@users.noreply.github.com>
2020-11-08 22:55:55 -0800

    Created Tables and SQLite3 Database

---
+++ Shared Class Notebook V1.0 CSE 111/createTables.sql
@@ -0,0 +1,43 @@
+CREATE TABLE account(
+            a_name VARCHAR(50) not null,
+            a_pass VARCHAR(50) not null,
+            a_ID INT not null,
+            a_type VARCHAR(10) not null);
+
+CREATE TABLE student(
+            s_name VARCHAR(50) not null,
+            s_ID INT not null,
+            s_age INT not null,
+            s_year INT not null);
+
+CREATE TABLE classesRegistered(
+            reg_cName VARCHAR(50) not null,
+            reg_cID INT not null,
+            reg_ID INT not null);
+
+CREATE TABLE classFolder(
+            class_name VARCHAR(50) not null,
+            class_cID INT not null,
+            class_ID INT not null,
+            class_lTime TEXT not null);
+
+CREATE TABLE notePages(
+            n_docName VARCHAR(50) not null,
+            n_timeStamp TEXT not null,
+            n_cID INT not null,
+            n_content TEXT not null);
+
+CREATE TABLE professor(
+            p_name VARCHAR(50) not null,
+            p_class VARCHAR(50) not null,
+            p_ID INT not null);            
+
+CREATE TABLE classRoster(
+            cl_name VARCHAR(50) not null,
+            cl_ID INT not null,
+            cl_cID INT not null);     
+
+CREATE TABLE classCatalog(
+            cla_name VARCHAR(50) not null,
+            cla_ID INT not null,
+            cla_cID INT not null);          
