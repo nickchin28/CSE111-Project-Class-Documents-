b56f1cfa597205f3a44cef9affc9c10a20de36a7
vmac <vmac@ucmerced.edu>
2020-11-29 23:41:27 -0800

    login/first part of prof access

--- Shared Class Notebook V1.0 CSE 111/phase2.py
+++ Shared Class Notebook V1.0 CSE 111/phase2.py
@@ -48,12 +48,14 @@ def login(_conn):
                 dist = ("select a_type from account where a_ID = ?")
                 cursor.execute(dist, [(i[3])])
                 fol = cursor.fetchall()
-                if fol == "prof":
-                    cla = ("select p_class from professor where p_name = ?")
+                if i[3] == "prof":
+                    cla = ("select * from professor where p_name = ?")
                     cursor.execute(cla, [(user)])
+                    fol = cursor.fetchall()
+                    for j in fol:
+                        profAccess(_conn, user , j[2])
                     
                     
-                    print("You are teaching: "+)
             return("exit")
         else:
             print("User or password is incorrect")
@@ -61,6 +63,32 @@ def login(_conn):
             if again.lower() == "n":
                 print("exited")
                 break   
+
+
+def profAccess(_conn, user, p_ID):
+    with sqlite3.connect("scnDatabase.sqlite") as data:
+        cursor = data.cursor()
+    while user:
+        print("1. What classes do I teach?")
+        print("2. Show class roster")
+        print("3. Check Registration requests")
+        print("4. View Class Notes")
+        print("4. View class catalog")
+        print("5. Logout")
+        choice = input("What would you like to do? ")
+
+        if choice == "1":
+            print("success")
+            cla = ("select p_class from professor where p_name = ? and p_ID = ?")
+            cursor.execute(cla, [user, p_ID])
+            okay = cursor.fetchall()
+            for i in okay: 
+                print("You are teaching: " + i[0])
+                print("\n")
+        
+        if choice == "2":
+            print("Success")
+    
 
 
 def main():
